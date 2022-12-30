88f900dc381a716645d52cc9acf731080e00a193
Humberto Rodrigues <hpereira@senseilabs.com>
2022-12-30 03:17:37 -0500

    fix file

--- src/Mellon.MultiTenant.Base/TenantSettings_99.cs
+++ src/Mellon.MultiTenant.Base/TenantSettings_99.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Mellon.MultiTenant.Base;
 
-public class TenantSettings2
+public class TenantSettings
 {
     private readonly MultiTenantSettings _multiTenantSettings;
 
