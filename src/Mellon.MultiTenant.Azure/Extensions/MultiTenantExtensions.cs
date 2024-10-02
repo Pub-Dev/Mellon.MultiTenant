542016bead85bccfe3703ccddae8378259187a2b
Humberto Rodrigues <hrodrigues@costmine.com>
2024-10-02 09:59:50 -0400

    refactor: adjust Mellon.Multitenant.Azure project - to work with the new editorconfig and update imports - update MultiTenantExtensions to use the namespace Microsoft.Extensions.DependencyInjection (#breaking change) - update the packages  - Azure.Identity from `1.10.4` to `1.12.1`  - Microsoft.Extensions.Configuration.AzureAppConfiguration from `7.0.0` to `7.3.0`

---
+++ src/Mellon.MultiTenant.Azure/Extensions/MultiTenantExtensions.cs
@@ -0,0 +1,36 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace Microsoft.Extensions.DependencyInjection;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+using Mellon.MultiTenant.Azure;
+using Mellon.MultiTenant.Base.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+public static class MultiTenantExtensions
+{
+	public static IServiceCollection AddMultiTenantAzureAppConfiguration(
+		this IServiceCollection services,
+		Action<AzureMultiTenantOptions> action = null)
+	{
+		services.AddSingleton(serviceProvider =>
+		{
+			var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+			var azureMultiTenantOptions = new AzureMultiTenantOptions
+			{
+				AzureAppConfigurationConnectionString = configuration["AzureAppConfigurationConnectionString"]
+			};
+
+			action?.Invoke(azureMultiTenantOptions);
+
+			return azureMultiTenantOptions;
+		});
+
+		services.RemoveAll<IMultiTenantConfigurationSource>();
+
+		services.AddSingleton<IMultiTenantConfigurationSource, AzureTenantSource>();
+
+		return services;
+	}
+}
