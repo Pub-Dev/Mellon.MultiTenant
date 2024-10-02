7b0792ff1da13c1f217d300c798cebbdae8a7d7b
Humberto Rodrigues <hrodrigues@costmine.com>
2024-10-02 09:53:53 -0400

    refactor: adjust Api sample to work with the new editorconfig and update imports

---
+++ samples/Api/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Entities;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Product
+{
+	[Key]
+	public int Id { get; set; }
+
+	public string Name { get; set; }
+
+	public int Quantity { get; set; }
+}
