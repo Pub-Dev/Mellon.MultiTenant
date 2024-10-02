bb790e40164fc1ece799b836a19ead46ab5c53c5
Humberto Rodrigues <hrodrigues@costmine.com>
2024-10-02 11:51:20 -0400

    refactor: adjust Mellon.Multitenant.ConfigServer project - to work with the new editorconfig and update imports - update ConfigServerTenantSource class to ConfigServerMultiTenantSource (#breaking change) - update MultiTenantExtensions to use the namespace Microsoft.Extensions.DependencyInjection (#breaking change) - update the packages 	- Steeltoe.Extensions.Configuration.ConfigServerCore from `3.2.5` to `3.2.8` 	- Steeltoe.Extensions.Configuration.PlaceholderCore from `3.2.5` to `3.2.8`

--- src/Mellon.MultiTenant.ConfigServer/ConfigServerMultiTenantSource.cs
+++ src/Mellon.MultiTenant.ConfigServer/ConfigServerMultiTenantSource.cs
@@ -1,26 +1,26 @@
-﻿using Mellon.MultiTenant.Base;
+﻿namespace Mellon.MultiTenant.ConfigServer;
+
+using Mellon.MultiTenant.Base;
 using Mellon.MultiTenant.Base.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Steeltoe.Extensions.Configuration.ConfigServer;
 using Steeltoe.Extensions.Configuration.Placeholder;
 
-namespace Mellon.MultiTenant.ConfigServer;
-
-public class ConfigServerTenantSource(
-    MultiTenantOptions multiTenantOptions,
-    IHostEnvironment hostEnvironment) : ITenantConfigurationSource
+public class ConfigServerMultiTenantSource(
+	MultiTenantOptions multiTenantOptions,
+	IHostEnvironment hostEnvironment) : IMultiTenantConfigurationSource
 {
-    public IConfigurationBuilder AddSource(
-        string tenant,
-        IConfigurationBuilder builder)
-    {
-        builder
-            .AddConfigServer(
-                hostEnvironment.EnvironmentName,
-                $"{multiTenantOptions.ApplicationName ?? hostEnvironment.ApplicationName}-{tenant}")
-            .AddPlaceholderResolver();
+	public IConfigurationBuilder AddSource(
+		string tenant,
+		IConfigurationBuilder builder)
+	{
+		builder
+			.AddConfigServer(
+				hostEnvironment.EnvironmentName,
+				$"{multiTenantOptions.ApplicationName ?? hostEnvironment.ApplicationName}-{tenant}")
+			.AddPlaceholderResolver();
 
-        return builder;
-    }
+		return builder;
+	}
 }
---
+++ src/Mellon.MultiTenant.ConfigServer/Extensions/MultiTenantExtensions.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace Microsoft.Extensions.DependencyInjection;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+using Mellon.MultiTenant.Base.Interfaces;
+using Mellon.MultiTenant.ConfigServer;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+public static class MultiTenantExtensions
+{
+	public static IServiceCollection AddMultiTenantSpringCloudConfig(
+		this IServiceCollection services)
+	{
+		services.RemoveAll<IMultiTenantConfigurationSource>();
+
+		services.AddSingleton<IMultiTenantConfigurationSource, ConfigServerMultiTenantSource>();
+
+		return services;
+	}
+}
